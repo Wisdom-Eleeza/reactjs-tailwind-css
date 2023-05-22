74456cbacbdd3e4f41055fd2ee41532ab9328c72
wisdom.eleeza@amalitech.com <wisdom.eleeza@amalitech.com>
2023-05-22 09:31:41 +0000

    React, tailwindcss bank_modern_app project

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ styles }) => {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[9px] ${styles}`}
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[10px] ${styles}`}
     >
       GetStarted
     </button>
